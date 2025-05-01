620a360f15730585483be8e93f2548389f7f4717
Shipul <shipul2910@gmail.com>
2025-05-01 22:30:37 +0530

    color added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react'; 
-import { ThemeProvider } from 'styled-components';
-import GlobalStyles from './components/styles/Global.styles';
-import Header from './components/Header';
-import HeroSection from './components/HeroSection';
-import Main from './components/Main';
-import Footer from './components/Footer';
+import React from "react";
+import { ThemeProvider } from "styled-components";
+import GlobalStyles from "./components/styles/Global.styles";
+import Header from "./components/Header";
+import HeroSection from "./components/HeroSection";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 
 const theme = {
   colors: {
     black: "black",
     white: "white",
+    blue: "red",
     cyan: "hsl(180, 66%, 49%)",
     darkViolet: "hsl(257, 27%, 26%)",
     red: "hsl(0, 87%, 67%)",
     gray: "hsl(0, 0%, 75%)",
     grayishViolet: "hsl(257, 7%, 63%)",
     veryDarkBlue: "hsl(255, 11%, 22%)",
-    veryDarkViolet: "hsl(260, 8%, 14%)"
+    veryDarkViolet: "hsl(260, 8%, 14%)",
   },
-  mobile: '375px',
-}
-
+  mobile: "375px",
+};
 
 function App() {
   return (
@@ -31,9 +31,7 @@ function App() {
       <Main />
       <Footer />
     </ThemeProvider>
-  )
+  );
 }
-
-
 
 export default App;
